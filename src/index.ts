cf76f0e390a50a71846674ed2130ac4a8e798d2d
Leonard Scheidemantel <leonard.scheidemantel@googlemail.com>
2020-11-16 20:02:07 +0100

    Move database connection from ormconfig to config.toml

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,11 @@ import {logger} from "./common";
 import * as api from "./api";
 import * as bot from "./bot";
 import {createConnection} from "typeorm";
-import {TelegramUser} from "./database/entity/TelegramUser";
-
 
 export function main() {
     logger.info("Starting up telegram service...");
-    createConnection()
+    createConnection({type: "postgres", host: config.get("database.postgres_host"), port: config.get("database.postgres_port"), username: config.get("database.postgres_user"), password: config.get("database.postgres_password"), database: config.get("database.postgres_db"), entities: [__dirname + "/database/entity/*{.js,.ts}"],
+        synchronize: true})
         .then(async (database) => {
 
             logger.info("Successfully connected to database");
